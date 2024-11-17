50d0cd0beb6b0a59bedd05a79c218b26237c9e81
dawlat <dawlathussien183@gmail.com>
2024-11-17 22:26:11 +0200

    fix: followup

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Layout from './Components/Layout/Layout';
 import About from './Components/About/About';
 import Contact from './Components/Contact/Contact';
 import Portfolio from './Components/Portfolio/Portfolio';
-import NotFound from './Components/Notfound/NotFound'
+import NotFound from './Components/Notfound/Notfound'
 import Home from "./Components/Home/Home";
 
 // routes
